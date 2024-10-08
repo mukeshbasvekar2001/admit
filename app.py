d6035eb4ef767970e4c051fff7e798aa4325817a
Rohan Rashinkar <65181638+rashinkarrohan@users.noreply.github.com>
2024-10-08 10:16:38 -0700

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import pandas as pd
 
 app = Flask(__name__)
@@ -10,17 +10,22 @@ CSV_FILE = 'database.csv'
 def index():
     data = pd.read_csv(CSV_FILE)
     records = data.to_dict(orient='records')
-    return render_template('index.html', records=records)
+    universities = data['university'].unique()
+    return render_template('index.html', records=records, universities=universities)
 
 @app.route('/update', methods=['GET', 'POST'])
 def update():
     if request.method == 'POST':
         university = request.form['university']
         course = request.form['course']
+        selection_status = request.form['selection_status']
         
-        # Convert GMAT and GRE scores to integers
-        gmat_score = int(request.form['gmat'])
-        gre_score = int(request.form['gre'])
+        # Convert GMAT and GRE scores to integers, default to None if not provided
+        gmat_score = request.form['gmat']
+        gmat_score = int(gmat_score) if gmat_score else None
+        
+        gre_score = request.form['gre']
+        gre_score = int(gre_score) if gre_score else None
         
         # Convert Experience and GPA to float
         experience_years = float(request.form['experience'])
@@ -31,41 +36,32 @@ def update():
 
         # Find the row to update
         row = data[(data['university'] == university) & (data['course'] == course)]
-        if not row.empty:
-            # Update GMAT
-            gmat_avg = row['gmat_average'].values[0]
-            gmat_count = row['gmat_count'].values[0]
+        
+        if not row.empty and selection_status == 'Selected':
+            new_data = {}
 
-            new_gmat_count = gmat_count + 1
-            new_gmat_avg = (gmat_avg * gmat_count + gmat_score) / new_gmat_count
+            if gmat_score is not None:
+                new_data['gmat_average'] = (row['gmat_average'].values[0] * row['gmat_count'].values[0] + gmat_score) / (row['gmat_count'].values[0] + 1)
+                new_data['gmat_count'] = row['gmat_count'].values[0] + 1
+            else:
+                new_data['gmat_average'] = row['gmat_average'].values[0]
+                new_data['gmat_count'] = row['gmat_count'].values[0]
 
-            # Update GRE
-            gre_avg = row['gre_average'].values[0]
-            gre_count = row['gre_count'].values[0]
+            if gre_score is not None:
+                new_data['gre_average'] = (row['gre_average'].values[0] * row['gre_count'].values[0] + gre_score) / (row['gre_count'].values[0] + 1)
+                new_data['gre_count'] = row['gre_count'].values[0] + 1
+            else:
+                new_data['gre_average'] = row['gre_average'].values[0]
+                new_data['gre_count'] = row['gre_count'].values[0]
 
-            new_gre_count = gre_count + 1
-            new_gre_avg = (gre_avg * gre_count + gre_score) / new_gre_count
-
-            # Update Experience
-            exp_avg = row['experience_average'].values[0]
-            exp_count = row['experience_count'].values[0]
-
-            new_exp_count = exp_count + 1
-            new_exp_avg = (exp_avg * exp_count + experience_years) / new_exp_count
-
-            # Update GPA
-            gpa_avg = row['gpa_average'].values[0]
-            gpa_count = row['gpa_count'].values[0]
-
-            new_gpa_count = gpa_count + 1
-            new_gpa_avg = (gpa_avg * gpa_count + gpa) / new_gpa_count
+            new_data['experience_average'] = (row['experience_average'].values[0] * row['experience_count'].values[0] + experience_years) / (row['experience_count'].values[0] + 1)
+            new_data['experience_count'] = row['experience_count'].values[0] + 1
+            
+            new_data['gpa_average'] = (row['gpa_average'].values[0] * row['gpa_count'].values[0] + gpa) / (row['gpa_count'].values[0] + 1)
+            new_data['gpa_count'] = row['gpa_count'].values[0] + 1
 
             # Update the DataFrame
-            data.loc[(data['university'] == university) & (data['course'] == course), 
-                      ['gmat_average', 'gmat_count', 'gre_average', 'gre_count', 
-                       'experience_average', 'experience_count', 'gpa_average', 'gpa_count']] = [
-                          new_gmat_avg, new_gmat_count, new_gre_avg, new_gre_count, 
-                          new_exp_avg, new_exp_count, new_gpa_avg, new_gpa_count]
+            data.loc[(data['university'] == university) & (data['course'] == course), list(new_data.keys())] = list(new_data.values())
 
             # Save the updated DataFrame back to CSV
             data.to_csv(CSV_FILE, index=False)
@@ -75,8 +71,13 @@ def update():
     # GET request to display the form
     data = pd.read_csv(CSV_FILE)
     universities = data['university'].unique()
-    courses = data['course'].unique()
-    return render_template('update.html', universities=universities, courses=courses)
+    return render_template('update.html', universities=universities)
+
+@app.route('/get_courses/<university>', methods=['GET'])
+def get_courses(university):
+    data = pd.read_csv(CSV_FILE)
+    courses = data[data['university'] == university]['course'].tolist()
+    return jsonify(courses)
 
 if __name__ == '__main__':
     app.run(debug=True)
